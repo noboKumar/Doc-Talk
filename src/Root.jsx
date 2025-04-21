8aaa31b7f3f646315857b859143e1b33a67c8391
noboKumar <nobokumar227@gmail.com>
2025-04-21 12:17:49 +0600

    add footer

--- src/Root.jsx
+++ src/Root.jsx
@@ -1,15 +1,18 @@
-import React from 'react';
-import NavBar from './components/NavBar';
-import Footer from './components/Footer';
-import { Outlet } from 'react-router';
+import React from "react";
+import NavBar from "./components/NavBar";
+import Footer from "./components/Footer";
+import { Outlet } from "react-router";
 
 const Root = () => {
-    return (
-        <div>
-            <NavBar></NavBar>
-            <Outlet></Outlet>
-        </div>
-    );
+  return (
+    <div>
+      <NavBar></NavBar>
+      <div className="min-h-[calc(100vh-465px)]">
+        <Outlet></Outlet>
+      </div>
+      <Footer></Footer>
+    </div>
+  );
 };
 
 export default Root;
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -5,7 +5,7 @@ import NavLinks from "./NavLinks";
 
 const NavBar = () => {
   return (
-    <div className="navbar bg-base-100 lg:w-11/12 mx-auto">
+    <div className="navbar bg-base-100 lg:w-11/12 mx-auto py-5">
       <div className="navbar-start">
         <div className="dropdown">
           <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
