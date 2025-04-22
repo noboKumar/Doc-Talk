97377d399ca8b62a79eb527216fe0a09a897eff4
noboKumar <nobokumar227@gmail.com>
2025-04-23 03:07:50 +0600

    add loading on all page

--- src/Root.jsx
+++ src/Root.jsx
@@ -1,18 +1,20 @@
 import NavBar from "./components/NavBar";
 import Footer from "./components/Footer";
-import { Outlet, useLocation } from "react-router";
+import { Outlet, useLocation, useNavigation } from "react-router";
+import Loading from "./components/Ui/Loading";
 
 const Root = () => {
   const ValidRoutes = ["/", "/my-bookings", "/blogs"];
   const location = useLocation();
   const currentPath = location.pathname;
   const isValidRoute = ValidRoutes.includes(currentPath);
+  const { state } = useNavigation();
 
   return (
     <div>
       <NavBar></NavBar>
       <div className="min-h-[calc(100vh-465px)]">
-        <Outlet></Outlet>
+        {state == "loading" ? <Loading /> : <Outlet />}
       </div>
       {isValidRoute && <Footer></Footer>}
     </div>
