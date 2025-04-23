6c21803b513afa7b150da2795d7b4d5cae61c6e1
noboKumar <nobokumar227@gmail.com>
2025-04-23 15:44:11 +0600

    fix countup

--- src/components/Ui/CountUpCards.jsx
+++ src/components/Ui/CountUpCards.jsx
@@ -21,7 +21,7 @@ const CountUpCards = () => {
         <div className="space-y-2 bg-white rounded-2xl px-12 py-10">
           <img className="mx-auto" src={Icon1} alt="" />
           <h1 className="text-6xl font-bold">
-            <CountUp enableScrollSpy="true" scrollSpyOnce="true" start={0} end={199} suffix="+" duration={5}></CountUp>
+            <CountUp start={0} end={199} suffix="+" duration={5}></CountUp>
           </h1>
           <p className="text-2xl text-[#0F0F0F99] font-semibold">
             Total Doctors
@@ -30,7 +30,7 @@ const CountUpCards = () => {
         <div className="space-y-2 bg-white rounded-2xl px-12 py-10">
           <img className="mx-auto" src={Icon2} alt="" />
           <h1 className="text-6xl font-bold">
-            <CountUp enableScrollSpy="true" scrollSpyOnce="true" start={0} end={467} suffix="+" duration={5}></CountUp>
+            <CountUp start={0} end={467} suffix="+" duration={5}></CountUp>
           </h1>
           <p className="text-2xl text-[#0F0F0F99] font-semibold">
             Total Reviews
@@ -39,14 +39,14 @@ const CountUpCards = () => {
         <div className="space-y-2 bg-white rounded-2xl px-12 py-10">
           <img className="mx-auto" src={Icon3} alt="" />
           <h1 className="text-6xl font-bold">
-            <CountUp enableScrollSpy="true" scrollSpyOnce="true" start={0} end={1900} suffix="+" duration={5}></CountUp>
+            <CountUp start={0} end={1900} suffix="+" duration={5}></CountUp>
           </h1>
           <p className="text-2xl text-[#0F0F0F99] font-semibold">Patients</p>
         </div>
         <div className="space-y-2 bg-white rounded-2xl px-12 py-10">
           <img className="mx-auto" src={Icon4} alt="" />
           <h1 className="text-6xl font-bold">
-            <CountUp enableScrollSpy="true" scrollSpyOnce="true" start={0} end={300} suffix="+" duration={5}></CountUp>
+            <CountUp start={0} end={300} suffix="+" duration={5}></CountUp>
           </h1>
           <p className="text-2xl text-[#0F0F0F99] font-semibold">
             Total Stuffs
